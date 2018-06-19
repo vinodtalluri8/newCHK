7659e2d49faf1dc4581a3e4f6635a3d765e976e9
vinodtalluri8 <vinodtalluri8@gmail.com>
2018-06-19 23:55:33 +0530

    fixes

--- src/app/checklist/reports/display-report/display-report.component.ts
+++ src/app/checklist/reports/display-report/display-report.component.ts
@@ -23,16 +23,58 @@ export class DisplayReportComponent implements OnInit {
     this.selectedRows = 15;
     this.exportFileName = 'SystemValuesList';
     this.dialogHeader = 'Add New Value';
-    this.systemCodelabelList=[
+    this.systemCodelabelList = [
       { field: 'evaluation', header: 'Evaluation' },
-      { field: 'Frequency', header: 'Frequency' },
-      { field: 'Checklist', header: 'Checklist' },
+      { field: 'frequency', header: 'Frequency' },
+      { field: 'checklist', header: 'Checklist' },
       { field: 'department', header: 'Department' },
-      { field: 'control', header: 'Control Length' },
-      { field: 'review', header: 'Review Length' },
+      { field: 'control', header: 'Control' },
+      { field: 'controlLength', header: 'Control Length' },
+      { field: 'reviewLength', header: 'Review Length' },
       { field: 'savings', header: 'Savings' }
     ];
-    this.sytemcodedatalist;
+    this.sytemcodedatalist = [
+      {
+      'evaluation': 'Candidate for Automation',
+      'frequency':'Ad Hoc',
+      'checklist':'AIM Currency Conversion',
+      'department':'Information Management',
+      'control': 'AIM Currency Conversion',
+      'controlLength':'50',
+      'reviewLength':'50',
+      'savings':'12.00'
+    },
+      {
+      'evaluation': 'Candidate for Automation',
+      'frequency':'Ad Hoc',
+      'checklist':'Analytic Support Ad Hoc',
+      'department':'Information Management',
+      'control': 'AIM Currency Conversion',
+      'controlLength':'50',
+      'reviewLength':'50',
+      'savings':'12.00'
+    },
+      {
+      'evaluation': 'Candidate for Automation',
+      'frequency':'Ad Hoc',
+      'checklist':'Analytic Support Ad Hoc',
+      'department':'Information Management',
+      'control': 'AIM Currency Conversion',
+      'controlLength':'50',
+      'reviewLength':'50',
+      'savings':'12.00'
+    },
+      {
+      'evaluation': 'Candidate for Automation',
+      'frequency':'Ad Hoc',
+      'checklist':'Analytic Support Ad Hoc',
+      'department':'Information Management',
+      'control': 'AIM Currency Conversion',
+      'controlLength':'50',
+      'reviewLength':'50',
+      'savings':'12.00'
+    }
+  ];
   }
 
   ngOnInit() {
